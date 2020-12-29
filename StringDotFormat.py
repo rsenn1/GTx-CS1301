1be18903c3bd90036e6d666795293f6a1eccf661
Rebecca Senn <rsenn1@users.noreply.github.com>
2020-12-28 19:50:49 -0600

    Create StringDotFormat.py

---
+++ StringDotFormat.py
@@ -0,0 +1,10 @@
+mystery_value_1 = 6
+mystery_value_2 = 3
+
+add = mystery_value_1 + mystery_value_2
+difference = mystery_value_1 - mystery_value_2
+product = mystery_value_1 * mystery_value_2
+quotient = mystery_value_1 / mystery_value_2
+
+my_string = "The sum is {0}, the difference is {1}, the product is {2}, and the quotient is {3}." .format(add, difference, product, quotient)
+print(my_string)
