f772cfc05505f83a7969e078550d5f92255e76f9
Rebecca Senn <rsenn1@users.noreply.github.com>
2021-01-13 19:56:07 -0600

    Create SnowDays.py

---
+++ SnowDays.py
@@ -0,0 +1,17 @@
+
+
+#It's snowing! 
+#Is school cancelled?
+
+mystery_state = "Georgia"
+
+if mystery_state =="New Jersey":
+    print("School isn't cancelled.")
+elif mystery_state =="North Carolina":
+    print("School is postponed.")
+elif mystery_state =="Georgia":
+    print("School is cancelled!")
+else:
+    print("idk wear a sweater")
+    
+
